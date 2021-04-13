ba332c6be99d55dd66e26fc627dd05a75864a1ca
Isak Strandberg <isakstrandberg@live.com>
2021-04-13 14:57:01 +0200

    almost done

--- D0041D lab 1/D0041D lab 1.cpp
+++ D0041D lab 1/D0041D lab 1.cpp
@@ -5,3 +5,26 @@
 #include "D0041D lab 1.h"
 using namespace std;
 
+void main() {
+	LinkedList test;
+
+	test.AddNode("math","Isak Strandberg");
+	test.AddNode("maths","petter");
+	test.AddNode("math","jesse");
+	test.AddNode("lunch", "jesse");
+	test.AddNode("lunch", "alexander");
+
+	test.Print();
+
+	test.Delete("lunch", "jesse");
+
+	test.Print();
+
+	test.Delete("bioshcik", "isak");
+	test.Delete("math", "Isak Strandberg");
+	test.Delete("math", "Isak Strandberg");
+	test.Delete("lunch", "alexander");
+	test.Delete("maths", "petter");
+
+	test.Print();
+}
--- D0041D lab 1/D0041D lab 1.h
+++ D0041D lab 1/D0041D lab 1.h
@@ -1,17 +1,30 @@
 
-
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-class Object {
+class Students {
 private:
 	vector<string> studentName;
-
 public:
-	string getStudentName(int number) {
-		return(studentName[number]);
+	void getStudentName() {
+		for(int i=0; i<studentName.size(); i++){
+			cout << studentName[i] << ", ";
+		}
+		cout << "\n";
+	}
+
+	string getSingleStudent(int i) {
+		return(studentName[i]);
+	}
+
+	int getSize() {
+		return(studentName.size());
+	}
+
+	void deleteStudent(int i) {
+		studentName.erase(studentName.begin() + i);
 	}
 
 	void addStudentName(string name) {
@@ -19,23 +32,18 @@ public:
 	}
 };
 
-class CourseNode {
-
+class StudentClass {
 private:
 	string courseName;
-	CourseNode* object;
-	CourseNode* next;
-	CourseNode* head;
-	CourseNode* tail;
-
 public:
-	CourseNode() {
+	StudentClass() {
 		string courseName = "";
-		CourseNode* object = NULL;
-		CourseNode* next = NULL;
-		CourseNode* head = NULL;
-		CourseNode* tail = NULL;
+		Students* student = nullptr;
+		StudentClass* next = nullptr;
 	};
+
+	StudentClass* next;
+	Students* object;
 
 	string getCourseName() {
 		return(courseName);
@@ -44,32 +52,142 @@ public:
 		courseName = cName;
 	}
 
-	CourseNode setObject() {
+	/*Students getObject() {
 		return(*object);
-	}
-	void setObject(CourseNode& o) {
+	}*/
+	void setObject(Students& o) {
 		object = &o;
 	}
 
-	CourseNode setNext() {
-		return(*next);
-	}
-	void getNext(CourseNode& n) {
+	/*CourseNode setNext(CourseNode& n) {
 		next = &n;
 	}
-
-	CourseNode setHead() {
-		return(*head);
-	}
-	void getHead(CourseNode& h) {
-		next = &h;
-	}
-
-	CourseNode setTail() {
-		return(*tail);
-	}
-	void getTail(CourseNode& t) {
-		next = &t;
-	}
-
+	CourseNode getNext() {
+		return(*next);
+	}*/
 };
+
+class LinkedList {
+private:
+	StudentClass* head;
+	StudentClass* tail;
+public:
+	LinkedList() {
+		StudentClass* head = nullptr;
+		StudentClass* tail = nullptr;
+	}
+
+	void AddNode(string name,string studentName) {
+		bool exist = false;
+		StudentClass* check;
+		check = head;
+		while (check != nullptr) {
+			if (check->getCourseName() == name) {
+				exist = true;
+				check->object->addStudentName(studentName);
+				cout << "Student has been added to the selected course \n";
+				break;
+			}
+			else {
+				check = check->next;
+			}
+		}
+
+		if (exist == false) {
+			StudentClass* temporary = new StudentClass;
+			temporary->setCourseName(name);
+			temporary->next = nullptr;
+			if (head == nullptr) {
+				head = temporary;
+				tail = temporary;
+			}
+			else {
+				tail->next = temporary;
+				tail = tail->next;
+			}
+			Students* newApplication = new Students;
+			newApplication->addStudentName(studentName);
+			temporary->setObject(*newApplication);
+			cout << "Student has been added to the selected course \n";
+		}
+	}
+
+	void Print() {
+		StudentClass* printTemp;
+		printTemp = head;
+		while (printTemp != nullptr) {
+			cout << "\n" << printTemp->getCourseName() << ": ";
+			printTemp->object->getStudentName();
+
+			printTemp = printTemp->next;
+		}
+	}
+
+	void Delete(string name, string student) {
+		bool exist = false;
+		StudentClass* check;
+		check = head;
+		while (check != nullptr) {
+			if (check->getCourseName() == name) {
+				exist = true;
+				break;
+			}
+			else {
+				check = check->next;
+			}
+		}
+
+		if (exist == false) {
+			cout << "Course does not exist \n";
+			return;
+		}
+		else {
+			int size = check->object->getSize();
+			for (int x = 0; x < size; x++) {
+				if (check->object->getSingleStudent(x) == student) {
+					check->object->deleteStudent(x);
+					break;
+				}
+				cout << "Student did not exist or is not in this course\n";
+			}
+		}
+	}
+};
+
+//	void Delete(string name) {
+//		bool exist = false;
+//		StudentClass* check;
+//		check = head;
+//		while (check != nullptr) {
+//			if (check->getCourseName() == name) {
+//				exist = true;
+//				break;
+//			}
+//			else {
+//				check = check->next;
+//			}
+//		}
+//
+//		if (exist == false) {
+//			cout << "Course does not exist \n";
+//		}
+//		else {
+//			StudentClass* findPrev;
+//			findPrev = head;
+//			while (findPrev != nullptr) {
+//				if (findPrev->next->getCourseName() == name) {
+//					break;
+//				}
+//				else {
+//					check = check->next;
+//				}
+//			}
+//			StudentClass* deleteTemp;
+//			deleteTemp = findPrev->next;
+//
+//			findPrev->next = deleteTemp->next;
+//			//delete Students
+//			delete deleteTemp;
+//		}
+//	}
+//};
